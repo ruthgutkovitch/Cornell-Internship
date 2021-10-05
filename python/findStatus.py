40e11c98e945636af8034717f182e557826e96ff
Ruth Goutkovitch <75093922+ruthgutkovitch@users.noreply.github.com>
2021-10-05 22:05:37 +0300

    Update findStatus.py

--- python/findStatus.py
+++ python/findStatus.py
@@ -8,16 +8,16 @@ fail, success = 0, 0
 w3 = Web3(Web3.WebsocketProvider("ws://127.0.0.1:8646"))
 print(w3.isConnected())
 
-#with open(file, 'r') as r:
-   # reader = pd.read_csv(r)
-    #for row in reader.itertuples():
-      #  hash = row.hash.lower()
-        #res = w3.eth.get_transaction_receipt(hash)
-        #if res[status]: success += 1
-        #else : fail += 1
+with open(file, 'r') as r:
+    reader = pd.read_csv(r)
+    for row in reader.itertuples():
+        hash = row.hash.lower()
+        res = w3.eth.get_transaction_receipt(hash)
+        if res[status]: success += 1
+        else : fail += 1
 
-#names =["Failed tx", "Successful"]
-#plt.pie([fail, success]) 
-#plt.title("Failed transactions")
-#plt.legend(labels = names, loc="center right" )
-#plt.savefig('/home/ruth/NFT-study/Ruth/Plots/failed_tx.png')
+names =["Failed tx", "Successful"]
+plt.pie([fail, success], autopct='%.2f') 
+plt.title("Failed transactions")
+plt.legend(labels = names)
+plt.savefig('/home/ruth/NFT-study/Ruth/Plots/failed_tx.png')
